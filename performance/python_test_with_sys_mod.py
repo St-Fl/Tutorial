4022dba99ddf8092bdd3b5391727b21695a1dd1d
Stefan Flachsbarth <sf@it-pl.de>
2017-03-19 15:43:16 +0100

    Update python_test_with_sys_mod.py

--- performance/python_test_with_sys_mod.py
+++ performance/python_test_with_sys_mod.py
@@ -12,5 +12,4 @@ sys.stdout = open(os.devnull, 'w')
 while LOOP < 1000000:
         if bar and not foo:
                 print LOOP
-                #os.system('echo "ok" >> /dev/null')
         LOOP=LOOP + 1
