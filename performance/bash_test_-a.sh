41307f22f37703b9ae64e4240dd8dff116d77903
Stefan Flachsbarth <sf@it-pl.de>
2017-03-19 13:21:28 +0100

    Create bash_test_-a.sh

---
+++ performance/bash_test_-a.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# vars
+LOOP=0
+bar="test"
+foo=""
+
+# while count to messure reasonable the performance
+while [ $LOOP -lt 1000000 ] ; do
+  # if [] && [] VS test -a - performance measurement 
+	#if [ -n "bar" ] && [ "foo" != "null" ]; then echo "ok" >> /dev/null ; fi
+	if test -n "bar" -a "foo" != "null"; then  echo "ok" >> /dev/null ; fi	
+  LOOP=$((LOOP + 1))
+done
